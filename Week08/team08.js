d6d1e8409e3c2f7567d1742207a7e7c1b7d183c4
AdamFinder <fin19011@byui.edu>
2021-11-06 13:32:27 -0700

    finall commit for week 8 team

--- Week08/team08.js
+++ Week08/team08.js
@@ -1,4 +1,4 @@
-const APP = {
+const SWAPI = {
     urls: {
         base: 'https://swapi.dev/api/',
         People: 'people/',
@@ -8,29 +8,29 @@ const APP = {
         Vehicles: 'vehicles/',
         Ships: 'starships/',
     },
-    init: () => {
-        APP.addListeners();
-        APP.buildNav();
+    start: () => {
+        SWAPI.addListeners();
+        SWAPI.addNav();
     },
-    addListeners: () => {
+    addListeners() {
         let nav = document.getElementById('nav');
-        nav.addEventListener('click', APP.getData);
-        footer.addEventListener('click', APP.getData);
+        nav.addEventListener('click', SWAPI.getData);
+        footer.addEventListener('click', SWAPI.getData);
     },
-    buildNav: () => {
+    addNav() {
         let df = new DocumentFragment();
-        for (let nm in APP.urls) {
+        for (let nm in SWAPI.urls) {
             if (nm != 'base') {
                 let link = document.createElement('a');
-                link.href = `${APP.urls.base}${APP.urls[nm]}`;
+                link.href = `${SWAPI.urls.base}${SWAPI.urls[nm]}`;
                 link.textContent = nm;
-                link.setAttribute('data-link', `${APP.urls.base}${APP.urls[nm]}`);
+                link.setAttribute('data-link', `${SWAPI.urls.base}${SWAPI.urls[nm]}`);
                 df.append(link);
             }
         }
         document.getElementById('nav').append(df);
     },
-    getData: (ev) => {
+    getData(ev) {
         if (ev) ev.preventDefault();
 
         let link = ev.target;
@@ -41,21 +41,41 @@ const APP = {
             if (!resp.ok) throw new Error(resp.statusText);
             return resp.json();
         })
-        .then(APP.buildList)
+        .then(SWAPI.buildList)
         .catch((err) => {
             console.error(err);
         });
     },
-    buildList: (data) => {
+    details(data, name){
+        let m = document.getElementById('main');
+        console.log(data);
+
+        let index = data.results.findIndex(x => item.name || item.title == name);
+        let details = data.results[index].entries();
+            m.innerHTML = details
+            .map((item) => {
+                    info = item.
+                    return `<p></p>`;
+                }
+            )
+            .join(' ');
+    },
+    buildList(data) {
         let m = document.getElementById('main');
         console.log(data);
 
         m.innerHTML = data.results
         .map((item) => {
             let nm = item.name || item.title;
-            return `<p>${nm}</p>`;
+            return `<p id="listItem">${nm}</p>`;
+            // let listItem = document.getElementById('listItem');
+            // listItem.addEventListener('click', details(data,nm));
+            // return `<p onclick=details(${data},${nm})>${nm}</p>`;
         })
         .join(' ');
+        // let listItem = document.getElementById('listItem');
+        // listItem.addEventListener('click', details(data, nm));
+
         let footer = document.getElementById('footer');
         footer.innerHTML = '';
 
@@ -65,9 +85,25 @@ const APP = {
             let url = new URL(data.previous);
             let labels = url.pathname.split('/');
             let label = labels[labels.length - 2];
-            prev.textContent = `<< Previous ${label}`;
+            prev.textContent = `⇐ Previous ${label}`;
             prev.setAttribute('data-link', data.previous);
             footer.append(prev);
+        }
+        let pages = Math.ceil(data.count/10);
+        for(i = 1; i <= pages; i++){
+            let link = document.createElement('a');
+            link.href = data.next;
+            let url = new URL(data.next);
+            index = url.href.lastIndexOf("/");
+            newurl = url.href.substr(0, index);
+            newurl += `/?page=${i}`;
+            link.href = newurl;
+            url = new URL(newurl);
+            let labels = url.pathname.split('/');
+            let label = labels[labels.length - 2];
+            link.textContent = `${i}`;
+            link.setAttribute('data-link', url);
+            footer.append(link);
         }
         if (data.next) {
             let next = document.createElement('a');
@@ -75,11 +111,11 @@ const APP = {
             let url = new URL(data.next);
             let labels = url.pathname.split('/');
             let label = labels[labels.length - 2];
-            next.textContent = `Next ${label} >>`;
+            next.textContent = `Next ${label} ⇒`;
             next.setAttribute('data-link', data.next);
             footer.append(next);
         }
     },
 };
 
-document.addEventListener('DOMContentLoaded', APP.init);
+document.addEventListener('DOMContentLoaded', SWAPI.start);
