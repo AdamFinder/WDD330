1e863aef4c8cc468aec1fb641db7f910974c81c7
AdamFinder <fin19011@byui.edu>
2021-12-01 21:13:45 -0700

    Edits

--- Final/js/cities.js
+++ Final/js/cities.js
@@ -1,21 +1,21 @@
-const url = "json/cities.json"
-const images = ['images/sedona_600_400', 'images/sodasprings.jpg', 'images/fishhaven.jpg']
+const url = "https://raw.githubusercontent.com/AdamFinder/WDD330/main/Final/json/cities.json"
+const images = ['images/sedona_600_400.jpg', 'images/sodasprings.jpg', 'images/fishhaven.jpg']
 
 fetch(url)
 .then(response => response.json())
 .then(jsonData => {
     const data = jsonData['cities']
-    const cities = data.filter(city => city.name === 'Fish Haven' || city.name === 'Soda Springs' || city.name === 'Preston')
+    const cities = data.filter(city => city.name === 'Sedona' || city.name === 'Soda Springs' || city.name === 'Preston')
     
     cities.forEach(city => {
-        const { name, motto, yearFounded, currentPopulation, averageRainfall } = city
+        const { name, motto, yearFounded, currentPopulation, topHikes } = city
 
         const card = document.createElement('section')
         const cityName = document.createElement('h2')
         const mottoText = document.createElement('h4')
         const year = document.createElement('div')
         const population = document.createElement('div')
-        const rain = document.createElement('div')
+        const hikes = document.createElement('div')
         const imageDiv = document.createElement('div')
         const txtWrapperDiv = document.createElement('div')
         const txtWithImage = document.createElement('div')
@@ -27,7 +27,7 @@ fetch(url)
         mottoText.textContent = motto
         year.innerHTML = `Year Founded: ${yearFounded}`
         population.innerHTML = `Population: ${currentPopulation}`
-        rain.innerHTML = `Annual Rain Fall: ${averageRainfall}`
+        hikes.innerHTML = `Top Hikes: ${topHikes}`
     
         card.appendChild(cityName)
         card.appendChild(mottoText)
@@ -35,7 +35,7 @@ fetch(url)
         txtWithImage.appendChild(txtWrapperDiv)
         txtWrapperDiv.appendChild(year)
         txtWrapperDiv.appendChild(population)
-        txtWrapperDiv.appendChild(rain)
+        txtWrapperDiv.appendChild(hikes)
         txtWithImage.appendChild(imageDiv)
 
         document.querySelector('div.wcontent').appendChild(card)
